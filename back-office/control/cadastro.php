d6c167e8aa09f55cc74bc2cafdee89956ad909f1
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2022-12-30 20:07:13 -0300

    core

--- back-office/control/cadastro.php
+++ back-office/control/cadastro.php
@@ -6,6 +6,12 @@ if(isset($_POST['submit']))
     print_r($_POST['sobrenome']);
     print_r($_POST['email']);
     print_r($_POST['senha']);
+    print_r($_POST['dia']);
+    print_r($_POST['mes']);
+    print_r($_POST['ano']);
+    print_r($_POST['options']);
+    print_r($_POST['pronome']);
+    print_r($_POST['genero']);
     
 }
 
